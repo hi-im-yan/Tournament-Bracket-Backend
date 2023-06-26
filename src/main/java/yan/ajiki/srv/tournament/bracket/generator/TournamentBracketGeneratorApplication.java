eb47d3c532cb45687da3056c40fd9bbdef513210
yan <yanajiki2010@hotmail.com>
2023-06-26 10:55:00 -0300

    first commit

---
+++ src/main/java/yan/ajiki/srv/tournament/bracket/generator/TournamentBracketGeneratorApplication.java
@@ -0,0 +1,13 @@
+package yan.ajiki.srv.tournament.bracket.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentBracketGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentBracketGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yan/ajiki/srv/tournament/bracket/generator/TournamentBracketGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package yan.ajiki.srv.tournament.bracket.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamentBracketGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
